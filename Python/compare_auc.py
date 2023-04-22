7a6e804eae7d16dfe47ce1fc219d213e4c2d0b5d
LuisSouto <luisouto.94@gmail.com>
2023-04-22 21:41:03 +0200

    Added LUNAR and Deep SVDD comparisons.

--- Python/compare_auc.py
+++ Python/compare_auc.py
@@ -16,7 +16,7 @@ from sklearn.metrics import roc_curve, roc_auc_score
 ## Load the data
 
 ffolder    = '../synthetic_data/'
-fname      = 'example_cross_1000_50'
+fname      = 'example_cross_1000_30'
 fresults   = '../results/'
 foutliers  = ffolder+fname+'_outliers.dat'
 fdata      = ffolder+fname+'_num.dat'
@@ -51,21 +51,31 @@ for score_t in score_type:
             auc_AIDA[iter,i] = roc_auc_score(y,scores_AIDA)
         iter += 1
 
-auc_IF   = np.zeros((n_exec,))
-auc_INNE = np.zeros((n_exec,))
+auc_IF    = np.zeros((n_exec,))
+auc_INNE  = np.zeros((n_exec,))
+auc_DeepSVDD = np.zeros((n_exec,))
+auc_LUNAR = np.zeros((n_exec,))
 for i in range(n_exec):
     fIF         = fresults+fname+'_IF_'+str(i+1)+'.dat'
     fINNE       = fresults+fname+'_INNE_'+str(i+1)+'.dat'
+    fDeepSVDD   = fresults+fname+'_DeepSVDD_'+str(i+1)+'.dat'
+    fLUNAR      = fresults+fname+'_LUNAR'+str(i+1)+'.dat'
     scores_IF   = pd.read_csv(fIF,skiprows=1,header=None,sep=" ",engine="python").to_numpy().squeeze()
     scores_INNE = pd.read_csv(fINNE,skiprows=1,header=None,sep=" ",engine="python").to_numpy().squeeze()
+    scores_DeepSVDD = pd.read_csv(fDeepSVDD,skiprows=1,header=None, sep=" ",engine="python").to_numpy().squeeze()
+    scores_LUNAR = pd.read_csv(fLUNAR,skiprows=1,header=None,sep=" ",engine="python").to_numpy().squeeze()
     auc_IF[i]   = roc_auc_score(y,scores_IF)
     auc_INNE[i] = roc_auc_score(y,scores_INNE)
+    auc_DeepSVDD[i] = roc_auc_score(y,scores_DeepSVDD)
+    auc_LUNAR[i] = roc_auc_score(y,scores_LUNAR)
 
 fpr_AIDA,tpr_AIDA,thresholds_AIDA = roc_curve(y,scores_AIDA)
 fpr_IF,tpr_IF,thresholds_IF       = roc_curve(y,scores_IF)
 fpr_LOF,tpr_LOF,thresholds_LOF    = roc_curve(y,scores_LOF)
 fpr_INNE,tpr_INNE,thresholds_INNE = roc_curve(y,scores_INNE)
 fpr_AVGKNN,tpr_AVGKNN,thresholds_AVGKNN = roc_curve(y,scores_AVGKNN)
+fpr_DeepSVDD,tpr_DeepSVDD,thresholds_DeepSVDD = roc_curve(y,scores_DeepSVDD)
+fpr_LUNAR,tpr_LUNAR,thresholds_LUNAR = roc_curve(y,scores_LUNAR)
 
 plt.figure()
 plt.plot(fpr_AIDA,tpr_AIDA,'b*-')
@@ -73,9 +83,11 @@ plt.plot(fpr_IF,tpr_IF,'kx-')
 plt.plot(fpr_LOF,tpr_LOF,'r^-')
 plt.plot(fpr_INNE,tpr_INNE,'go-')
 plt.plot(fpr_AVGKNN,tpr_AVGKNN,'ms-')
+plt.plot(fpr_DeepSVDD,tpr_DeepSVDD,'cp-')
+plt.plot(fpr_LUNAR,tpr_LUNAR,'yP-')
 plt.xlabel('FPR')
 plt.ylabel('TPR')
-plt.legend(('AIDA','IF','LOF','INNE','AVGKNN'))
+plt.legend(('AIDA','IF','LOF','INNE','AVGKNN','DeepSVDD','LUNAR'))
 plt.show()
 
 auc_LOF    = roc_auc_score(y,scores_LOF)
@@ -86,6 +98,8 @@ for i in range(aida_types):
     print('AUC AIDA '+AIDAtypes[i]+':',format(auc_AIDA[i].mean(),"0.3f"),format(auc_AIDA[i].std(),"0.3f"))
 print('AUC IF:     ',format(auc_IF.mean(),"0.3f"),format(auc_IF.std(),"0.3f"))
 print('AUC INNE:   ',format(auc_INNE.mean(),"0.3f"),format(auc_INNE.std(),"0.3f"))
+print('AUC DeepSVDD:',format(auc_DeepSVDD.mean(),"0.3f"),format(auc_DeepSVDD.std(),"0.3f"))
+print('AUC LUNAR:   ',format(auc_LUNAR.mean(),"0.3f"),format(auc_LUNAR.std(),"0.3f"))
 print('AUC LOF:    ',format(auc_LOF,"0.3f"))
 print('AUC AVGKNN: ',format(auc_AVGKNN,"0.3f"))
 
--- Python/test_sklearn.py
+++ Python/test_sklearn.py
@@ -13,44 +13,86 @@ from sklearn.ensemble import IsolationForest
 from sklearn.neighbors import LocalOutlierFactor
 from pyod.models.inne import INNE
 from pyod.models.knn import KNN
+from pyod.models.lunar import LUNAR
+from pyod.models.deep_svdd import DeepSVDD
 
 ## Compute anomaly scores
 
-tname     = 'example_cross_1000_50'
-name      = "../synthetic_data/"+tname
-filename  = name+"_num.dat"
+tname = 'example_cross_1000_30'
+name = "../synthetic_data/"+tname
+filename = name+"_num.dat"
 file_info = name+"_outliers.dat"
-n,nF      = pd.read_csv(filename,nrows=1,header=None,sep=",").to_numpy().squeeze()
-X         = pd.read_csv(filename,skiprows=1,header=None,sep=",",engine='python').to_numpy()
-
-n_outliers = pd.read_csv(file_info,nrows=1,header=None).to_numpy().squeeze()
-outliers   = pd.read_csv(file_info,skiprows=1,header=None,sep=" ",engine="python").to_numpy().squeeze()
+n,nF = pd.read_csv(filename,
+                   nrows=1,
+                   header=None,
+                   sep=","
+                   ).to_numpy().squeeze()
+X = pd.read_csv(filename,
+                skiprows=1,
+                header=None,
+                sep=",",
+                engine='python'
+                ).to_numpy()
+n_outliers = pd.read_csv(file_info,
+                         nrows=1,
+                         header=None
+                         ).to_numpy().squeeze()
+outliers   = pd.read_csv(file_info, skiprows=1,
+                         header=None,
+                         sep=" ",
+                         engine="python"
+                         ).to_numpy().squeeze()
 
 for i in range(10):
-    iForest = IsolationForest(random_state=i,max_samples=256,n_estimators=100).fit(X)
-    scores  = -iForest.score_samples(X)
-    scores  = (scores-scores.mean())/scores.std()
-    fres_IF = open('../results/'+tname+'_IF_'+str(i+1)+'.dat','w')
-    print(n,file=fres_IF)
+    iForest = IsolationForest(random_state=i,
+                              max_samples=256,
+                              n_estimators=100
+                              ).fit(X)
+    scores = -iForest.score_samples(X)
+    scores = (scores - scores.mean()) / scores.std()
+    fres_IF = open('../results/' + tname + '_IF_' + str(i+1) + '.dat', 'w')
+    print(n, file=fres_IF)
     for j in range(n):
-        print(scores[j],file=fres_IF)
+        print(scores[j], file=fres_IF)
     fres_IF.close()
 
-    inne = INNE(n_estimators=100,random_state=i,max_samples=8)
+    inne = INNE(n_estimators=100, random_state=i, max_samples=8)
     inne.fit(X)
     scores_inne = inne.decision_function(X)
-    scores_inne = (scores_inne-scores_inne.mean())/scores_inne.std()
-    fres_inne = open('../results/'+tname+'_INNE_'+str(i+1)+'.dat','w')
-    print(n,file=fres_inne)
+    scores_inne = (scores_inne - scores_inne.mean()) / scores_inne.std()
+    fres_inne = open('../results/' + tname + '_INNE_' + str(i+1) + '.dat', 'w')
+    print(n, file=fres_inne)
     for j in range(n):
-        print(scores_inne[j],file=fres_inne)
+        print(scores_inne[j], file=fres_inne)
     fres_inne.close()
 
-LOF1 = LocalOutlierFactor(p=1,n_neighbors=min(20,int(0.05*n)))
+    deep_svdd = DeepSVDD(batch_size=32, random_state=i, epochs=100)
+    deep_svdd.fit(X)
+    scores_DeepSVDD = deep_svdd.decision_function(X)
+
+    fres_deep_svdd = open('../results/' + tname + '_DeepSVDD_' + str(i+1)
+                          + '.dat', 'w')
+    print(n,file=fres_deep_svdd)
+    for j in range(n):
+        print(scores_DeepSVDD[j],file=fres_deep_svdd)
+    fres_deep_svdd.close()
+
+    lunar = LUNAR(n_neighbours=5)
+    lunar.fit(X)
+    scores_LUNAR = lunar.decision_function(X)
+
+    fres_lunar = open('../results/' + tname + '_LUNAR' + str(i+1)
+                      + '.dat', 'w')
+    print(n, file=fres_lunar)
+    for j in range(n):
+        print(scores_LUNAR[j], file=fres_lunar)
+    fres_lunar.close()
+
+LOF1 = LocalOutlierFactor(p=1, n_neighbors=min(20, int(0.05 * n)))
 LOF1.fit(X)
 scores_LOF = -LOF1.negative_outlier_factor_
 
-avgknn = KNN(p=1,n_neighbors=min(20,int(0.05*n)),method='mean')
+avgknn = KNN(p=1, n_neighbors=min(20, int(0.05 * n)), method='mean')
 avgknn.fit(X)
 scores_avgknn = avgknn.decision_function(X)
 
